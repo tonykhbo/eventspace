9199e3f26d283a7f6d4bd68f3a60c7c199992eb3
xjanua <tonybo@live.unc.edu>
2014-03-01 18:05:32 -0500

    added onclicklistener method to mainactivity

--- EventSpace/src/com/example/eventspace/MainActivity.java
+++ EventSpace/src/com/example/eventspace/MainActivity.java
@@ -1,14 +1,14 @@
 package com.example.eventspace;
 
-//TEST COMMENT - JON
-
 import com.pushbots.push.Pushbots;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 	private String SENDER_ID = "297016747626";
 	private String APPLICATION_ID = "531245c11d0ab1292a8b4607";
 
@@ -17,7 +17,6 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		Pushbots.init(this, SENDER_ID, APPLICATION_ID);
 		setContentView(R.layout.activity_main);
-		//hi ok last time
 	}
 
 	@Override
@@ -26,6 +25,11 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	//Yo, this is Andy's comment.
+
+	@Override
+	public void onClick(DialogInterface dialog, int which) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
