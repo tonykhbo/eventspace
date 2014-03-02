7f4d137a5d36ccb434a32c9b807e7eecdcf577f2
s-tyler-smith <stephen.ty.smith@gmail.com>
2014-03-01 20:02:22 -0500

    fixed layout

--- EventSpace/gen/com/example/eventspace/R.java
+++ EventSpace/gen/com/example/eventspace/R.java
@@ -25,9 +25,9 @@ public final class R {
     }
     public static final class id {
         public static final int action_settings=0x7f080005;
-        public static final int contactInfoButton=0x7f080004;
-        public static final int mapButton=0x7f080003;
-        public static final int publicChatButton=0x7f080002;
+        public static final int contactInfoButton=0x7f080003;
+        public static final int mapButton=0x7f080002;
+        public static final int publicChatButton=0x7f080004;
         public static final int scheduleButton=0x7f080001;
         public static final int textArea=0x7f080000;
     }
